3ba5630463488963c5509cb151600461a2d50c2d
Anuja005 <168641152+Anuja005@users.noreply.github.com>
2026-07-24 20:45:11 +0530

    create Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,91 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../css/style.css";
+
+
 function Register(){
 
     return(
 
-        <h1>
-            Register Page
-        </h1>
+        <>
+
+        <Navbar />
+
+
+        <section className="register-page">
+
+
+            <div className="register-box">
+
+
+                <h1>
+                    Create Account
+                </h1>
+
+
+                <p>
+                    Join EstateHub today
+                </p>
+
+
+
+                <input
+                    type="text"
+                    placeholder="Full Name"
+                />
+
+
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                />
+
+
+                <input
+                    type="tel"
+                    placeholder="Mobile Number"
+                />
+
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                />
+
+
+
+                <button>
+                    Register
+                </button>
+
+
+
+                <p className="login-link">
+
+                    Already have an account?
+
+                    <span
+                    onClick={() => window.location.href="/login"}
+                    >
+                        Login
+                    </span>
+
+                </p>
+
+
+            </div>
+
+
+        </section>
+
+
+        <Footer />
+
+        </>
 
     );
 
 }
+
 
 export default Register;

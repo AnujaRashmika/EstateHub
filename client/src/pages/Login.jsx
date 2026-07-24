ea9dd37661954675aa119e666d3ca8eff7d0d815
Anuja005 <168641152+Anuja005@users.noreply.github.com>
2026-07-24 20:39:43 +0530

    create Login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,98 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../css/style.css";
+
+
 function Login(){
 
     return(
 
-        <h1>
-            Login Page
-        </h1>
+        <>
+
+        <Navbar />
+
+
+        <section className="auth-page">
+
+
+            <div className="auth-box">
+
+
+                <h1>
+                    Welcome Back
+                </h1>
+
+
+                <p>
+                    Login to your EstateHub account
+                </p>
+
+
+                <input 
+                    type="email"
+                    placeholder="Email Address"
+                />
+
+
+                <input 
+                    type="password"
+                    placeholder="Password"
+                />
+
+
+                <div className="remember">
+
+
+                    <label>
+
+                        <input type="checkbox"/>
+
+                         Remember me
+
+                    </label>
+
+
+                    <span>
+                        Forgot Password?
+                    </span>
+
+
+                </div>
+
+
+
+                <button>
+                    Login
+                </button>
+
+
+
+                <p className="register-text">
+
+                    Don't have an account?
+
+                    <span
+                    onClick={() => window.location.href="/register"}
+                    >
+                        Register
+                    </span>
+
+                </p>
+
+
+            </div>
+
+
+        </section>
+
+
+        <Footer />
+
+        </>
 
     );
 
 }
+
 
 export default Login;

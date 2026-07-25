5d8f969d9a22bd2ef033a08e419703b7cabd8fc3
Anuja005 <168641152+Anuja005@users.noreply.github.com>
2026-07-25 22:57:13 +0530

    fixed mongodb connection failed issue

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,33 @@
 const mongoose = require("mongoose");
 
+
 const connectDB = async () => {
 
     try {
 
-        await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(
+            process.env.MONGODB_URI
+        );
 
-        console.log("MongoDB Connected");
+
+        console.log(
+            `MongoDB Connected: ${conn.connection.host}`
+        );
+
 
     } catch (error) {
 
-        console.log(error.message);
+        console.error(
+            "MongoDB Connection Error:",
+            error.message
+        );
+
 
         process.exit(1);
 
     }
 
 };
+
 
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,30 +1,54 @@
+const dns = require("dns");
+
+
+// Fix MongoDB SRV DNS issue
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4"
+]);
+
+dns.setDefaultResultOrder("ipv4first");
+
+
+require("dotenv").config();
+
+
 const express = require("express");
 const cors = require("cors");
+
+const connectDB = require("./config/db");
 const propertyRoutes = require("./routes/propertyRoutes");
-require("dotenv").config();
 
 
 const app = express();
 
+
+// MongoDB Connection
 connectDB();
 
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/properties",propertyRoutes);
 
 
-app.get("/", (req,res)=>{
+// Routes
+app.use("/api/properties", propertyRoutes);
+
+
+// Test Route
+app.get("/", (req, res) => {
 
     res.send("EstateHub Backend Running");
 
 });
 
 
-
+// Server
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`);
 
